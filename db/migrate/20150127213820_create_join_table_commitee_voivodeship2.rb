9ce70d5626f00b81cc6199782f2b5238ebc61075
Aneta Pityńska <aneta.pitynska@no-restrictions.com>
2015-03-03 21:18:03 +0100

    db restore

--- db/migrate/20150127213820_create_join_table_commitee_voivodeship2.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCommiteeVoivodeship < ActiveRecord::Migration
-  def change
-  	 create_table :commitees_voivodeships, id: false do |t|
-        t.integer :commitee_id
-      t.integer :voivodeship_id
-   
-  end
-end
